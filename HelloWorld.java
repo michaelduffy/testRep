57a88647404b7a3084cfa8a172a3fb0669202bbc
michaelduffy <mictriduf@hotmail.co.uk>
2017-03-24 16:30:10 +0000

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package gitVersionTest;
+
+public class HelloWorld {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This is version 1 line");
+		System.out.println("This is version 2 line");
+
+	}
+
+}
