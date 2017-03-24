39a8a368403bfef0235cb3b16c3b291f65601b0b
michaelduffy <mictriduf@hotmail.co.uk>
2017-03-24 16:42:19 +0000

    Add files via upload

---
+++ Class2.java
@@ -0,0 +1,11 @@
+package gitVersionTest;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		System.out.println("class2 version 1 added");
+		System.out.println("class2 version 2 added");
+
+	}
+
+}
---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package gitVersionTest;
+
+public class HelloWorld {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This is version 1 line");
+		System.out.println("This is version 2 line");
+		System.out.println("This is version 3 line");
+		System.out.println("This is version 4 line");
+		System.out.println("This is version 5 line");
+
+	}
+
+}
