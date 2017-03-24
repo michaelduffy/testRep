88e5202b811ac4285ba64e25f7d0326195c2ec07
michaelduffy <mictriduf@hotmail.co.uk>
2017-03-24 16:44:10 +0000

    Update Class2.java

--- Class2.java
+++ Class2.java
@@ -4,7 +4,7 @@ public class Class2 {
 
 	public static void main(String[] args) {
 		System.out.println("class2 version 1 added");
-		System.out.println("class2 version 2 added");
+		System.out.println("class2 version 2a added");
 
 	}
 
